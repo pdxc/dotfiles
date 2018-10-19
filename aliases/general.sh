8dedcec43df88a55391883746d4013c4a915c8c7
Paul Diebold <paul.diebold@expel.io>
2018-10-19 15:47:55 -0400

    Fix formatting in general aliases file

--- aliases/general.sh
+++ aliases/general.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-alias aliases="v ~/dotfiles/aliases"
+alias aliases="vim ~/dotfiles/aliases"
 
 function of() {
-		vim -O $(ag -l $1)
-	}
+	vim -O $(ag -l $1)
+}
 alias of=of
 
 function off() {
-		vim -O $(ag -lg $1)
-	}
+	vim -O $(ag -lg $1)
+}
 alias off=off
 
