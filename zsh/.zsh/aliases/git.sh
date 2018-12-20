3dd3c6203f594055c60d61f762d1ca55ffc910e3
Paul Diebold <paul.diebold@expel.io>
2018-12-20 11:26:26 -0500

    Improve git aliases
    Add gs and fp. Change ogd so that the vim git-gutter plugin will use the
    base commit of the git diff

--- zsh/.zsh/aliases/git.sh
+++ zsh/.zsh/aliases/git.sh
@@ -1,12 +1,36 @@
 #!/bin/sh
 
-funciton gdno() {
+function gs() {
+	git status $@
+}
+
+function fp() {
+	if [ $# -eq 0 ]; then
+		git merge-base --fork-point master
+	else
+		git merge-base --fork-point $@
+	fi
+}
+
+function gdc() {
+	git diff --relative --name-only --diff-filter=U
+}
+
+function ogdc() {
+	e $(git diff --relative --name-only --diff-filter=U)
+}
+
+function gdno() {
 	git diff --relative --name-only $@
 }
-alias gdno=gdno
 
 function ogd() {
-	e $(git diff --relative --name-only $@)
+	if [ $(git rev-parse -q --verify $1) ]; then
+		echo $(git diff --relative --name-only $@)
+		# https://github.com/airblade/vim-gitgutter "you can force vim-gitgutter to update its signs across all visible buffers with :GitGutterAll"
+		vim -p $(git diff --relative --name-only $@) -c ":let g:gitgutter_diff_base='$1'|:GitGutterAll"
+	else
+		e $(git diff --relative --name-only $@)
+	fi
 }
-alias ogd=ogd
 
