6e1088bc3834720867cb3611c572cb26f216e493
Paul Diebold <paul.diebold@expel.io>
2018-12-21 11:13:24 -0500

    Improve of() shell function

---
+++ zsh/.zsh/aliases/general.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+alias aliases="vim ~/.zsh/aliases"
+alias secrets="vim ~/.zsh/secrets"
+
+alias vrc="vim ~/.vimrc"
+alias zrc="vim ~/.zshrc"
+alias trc="vim ~/.tmux.conf"
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# Always ls after cd
+c() {
+	cd $1 && ls -lah
+}
+alias cd=c
+
+of() {
+	## Alternate version that opens a tab for each matching line
+	#local command=""
+	#local separator=""
+	#for i in $(ag --column ${@} | cut -d ':' -f 1-3); do
+	#	local f=$(echo ${i} | cut -d ':' -f 1)
+	#	local l=$(echo ${i} | cut -d ':' -f 2)
+	#	local c=$(echo ${i} | cut -d ':' -f 3)
+	#	command="${command}${separator}:e ${f}|:let @/="\""${@}"\""|:set hls|:call cursor(${l},${c})"
+	#	separator="|:tabnew |"
+	#done
+	#echo ${command}
+	#vim -p -c "${command}"
+
+	local to_print=""
+	local command=""
+	local separator=""
+	for i in $(ag -l $@); do
+		to_print="${to_print} ${i}"
+		command="${command}${separator}:e ${i}|:execute "\""normal\! G\$/${@}\\<cr>"\"
+		separator="|:tabnew |"
+	done
+	echo ${to_print}
+	vim -p -c "${command}"
+}
+
+off() {
+	e $(ag -lg $@)
+}
+
